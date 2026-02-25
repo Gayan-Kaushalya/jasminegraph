f14819cfb6f676ece4be00095e6761d9b0466a89
Gayan Kaushalya <gaykaush@gmail.com>
2026-02-25 12:58:26 +0530

    Minor changes

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Clean stale CMake build artifacts to avoid conflicts with
+# pre-existing build files from the base Docker image
+rm -rf CMakeCache.txt CMakeFiles/
+
 if [ "$1" = "--debug" ]; then
-    cmake -DCMAKE_ENABLE_DEBUG=1 clean .
+    cmake -DCMAKE_ENABLE_DEBUG=1 .
 else
-    cmake clean .
+    cmake .
 fi
 cmake --build . --target JasmineGraph -- -j 4
